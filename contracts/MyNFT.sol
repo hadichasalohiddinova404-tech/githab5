425c6aab0285e7a63ec89f759a97235711fa88eb
hadichasalohiddinova404-tech <hadichasalohiddinova404@gmail.com>
2026-04-15 20:28:53 +0500

    Create MyNFT.sol

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    event NFTMinted(address indexed minter, uint256 tokenId, string tokenURI);
+
+    constructor(address initialOwner)
+        ERC721("PremiumNFT", "PNFT")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        emit NFTMinted(to, tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
